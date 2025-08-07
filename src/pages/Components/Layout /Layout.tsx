ae3bd9522a6f5d69f0d81cd036fc536d66f36b0d
yujin5959 <jinjin17104@naver.com>
2025-08-07 12:32:01 +0900

    feat: layout 개발

--- src/pages/Components/Layout /Layout.tsx
+++ src/pages/Components/Layout /Layout.tsx
@@ -1,9 +1,17 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from '../LayoutComponents/Navbar';
+import Footer from '../LayoutComponents/Footer';
 
 export default function Layout() {
     return (
-        <div>
-            <Outlet />
+        <div className="flex flex-col min-h-screen">
+            <Navbar />
+
+            <main className="flex-1">
+                <Outlet /> {/* 또는 Routes */}
+            </main>
+
+            <Footer />
         </div>
     );
 }
