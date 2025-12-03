c73fc6edc43e6d6c98c407a3bfea396b65825b25
JANGJEEYEON <jjiyeon27@gmail.com>
2025-12-03 20:04:44 +0900

    fix: 타입 import 오류 수정

--- src/api/home/home.ts
+++ src/api/home/home.ts
@@ -1,11 +1,11 @@
-import type { TexhibitionCard } from '@/types/home/home';
+import type { Iexhibition } from '@/types/home/home';
 import { axiosInstance } from '../axiosInstance';
 
-export const getartierecommended = async (): Promise<TexhibitionCard> => {
-    const res = await axiosInstance.get<TexhibitionCard>('/api/v1/exhibitions/artie-recommendation');
+export const getartierecommended = async (): Promise<Iexhibition> => {
+    const res = await axiosInstance.get<Iexhibition>('/api/v1/exhibitions/artie-recommendation');
     return res.data;
 };
-export const gethotnow = async (): Promise<TexhibitionCard> => {
-    const res = await axiosInstance.get<TexhibitionCard>('/api/v1/exhibitions/hot-now');
+export const gethotnow = async (): Promise<Iexhibition> => {
+    const res = await axiosInstance.get<Iexhibition>('/api/v1/exhibitions/hot-now');
     return res.data;
 };
